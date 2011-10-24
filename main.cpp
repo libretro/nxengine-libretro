7eba23eccfd07c523e3ad30c4d0122f161a23124
TwinAphex51224 <twinaphex1@gmail.com>
2011-10-24 23:08:56 +0200

    More scrubbing out of debug code - CPU usage might now be somewhat lesser

--- main.cpp
+++ main.cpp
@@ -17,33 +17,37 @@ static uint32_t fpstimer = 0;
 
 #define GAME_WAIT			(1000/GAME_FPS)	// sets framerate
 int framecount = 0;
+#ifdef DEBUG
 bool freezeframe = false;
+#endif
 int flipacceltime = 0;
 
 int main(int argc, char *argv[])
 {
-bool inhibit_loadfade = false;
-bool error = false;
-bool freshstart;
-	
+	bool inhibit_loadfade = false;
+	bool error = false;
+	bool freshstart;
+
+	#ifdef USE_LOGGING
 	SetLogFilename("debug.txt");
+	#endif
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0)
 	{
 		staterr("ack, sdl_init failed: %s.", SDL_GetError());
 		return 1;
 	}
 	atexit(SDL_Quit);
-	
+
 	// start up inputs first thing because settings_load may remap them
 	input_init();
-	
+
 	// load settings, or at least get the defaults,
 	// so we know the initial screen resolution.
 	settings_load();
-	
+
 	if (Graphics::init(settings->resolution)) { staterr("Failed to initilize graphics."); return 1; }
 	if (font_init()) { staterr("Failed to load font."); return 1; }
-	
+
 	if (!settings->files_extracted)
 	{
 		if (extract_main())
@@ -58,47 +62,48 @@ bool freshstart;
 			settings_save();
 		}
 	}
-	
+
 	if (check_data_exists())
 	{
 		return 1;
 	}
-	
+
 	//Graphics::ShowLoadingScreen();
 	if (sound_init()) { fatal("Failed to initilize sound."); return 1; }
 	if (trig_init()) { fatal("Failed trig module init."); return 1; }
-	
+
 	if (tsc_init()) { fatal("Failed to initilize script engine."); return 1; }
 	if (textbox.Init()) { fatal("Failed to initialize textboxes."); return 1; }
 	if (Carets::init()) { fatal("Failed to initialize carets."); return 1; }
-	
+
 	if (game.init()) return 1;
 	game.setmode(GM_NORMAL);
 	// set null stage just to have something to do while we go to intro
 	game.switchstage.mapno = 0;
-	
+
 	//#define REPLAY
-	#ifdef REPLAY
-		game.switchstage.mapno = START_REPLAY;
-		Replay::set_ffwd(23400);
-		//Replay::set_stopat(3500);
-	#else
-		//game.switchstage.mapno = LOAD_GAME;
-		//game.pause(GP_OPTIONS);
-		
-		if (settings->skip_intro && file_exists(GetProfileName(settings->last_save_slot)))
-			game.switchstage.mapno = LOAD_GAME;
-		else
-			game.setmode(GM_INTRO);
-	#endif
-	
+#ifdef REPLAY
+	game.switchstage.mapno = START_REPLAY;
+	Replay::set_ffwd(23400);
+	//Replay::set_stopat(3500);
+#else
+	//game.switchstage.mapno = LOAD_GAME;
+	//game.pause(GP_OPTIONS);
+
+	if (settings->skip_intro && file_exists(GetProfileName(settings->last_save_slot)))
+		game.switchstage.mapno = LOAD_GAME;
+	else
+		game.setmode(GM_INTRO);
+#endif
+
 	// for debug
 	if (game.paused) { game.switchstage.mapno = 0; game.switchstage.eventonentry = 0; }
 	if (game.switchstage.mapno == LOAD_GAME) inhibit_loadfade = true;
-	
+
 	game.running = true;
 	freshstart = true;
-	while(game.running)
+
+	do
 	{
 		// SSS/SPS persists across stage transitions until explicitly
 		// stopped, or you die & reload. It seems a bit risky to me,
@@ -107,30 +112,30 @@ bool freshstart;
 		{
 			StopLoopSounds();
 		}
-		
+
 		// enter next stage, whatever it may be
 		if (game.switchstage.mapno == LOAD_GAME || \
-			game.switchstage.mapno == LOAD_GAME_FROM_MENU)
+				game.switchstage.mapno == LOAD_GAME_FROM_MENU)
 		{
 			if (game.switchstage.mapno == LOAD_GAME_FROM_MENU)
 				freshstart = true;
-			
+
 			stat("= Loading game =");
 			if (game_load(settings->last_save_slot))
 			{
 				fatal("savefile error");
 				goto ingame_error;
 			}
-			
+
 			Replay::OnGameStarting();
-			
+
 			if (!inhibit_loadfade) fade.Start(FADE_IN, FADE_CENTER);
 			else inhibit_loadfade = false;
 		}
 		else if (game.switchstage.mapno == START_REPLAY)
 		{
 			stat(">> beginning replay '%s'", GetReplayName(game.switchstage.param));
-			
+
 			StopScripts();
 			if (Replay::begin_playback(GetReplayName(game.switchstage.param)))
 			{
@@ -141,83 +146,90 @@ bool freshstart;
 		else
 		{
 			if (game.switchstage.mapno == NEW_GAME || \
-				game.switchstage.mapno == NEW_GAME_FROM_MENU)
+					game.switchstage.mapno == NEW_GAME_FROM_MENU)
 			{
 				bool show_intro = (game.switchstage.mapno == NEW_GAME_FROM_MENU);
 				InitNewGame(show_intro);
 			}
-			
+
 			// slide weapon bar on first intro to Start Point
 			if (game.switchstage.mapno == STAGE_START_POINT && \
-				game.switchstage.eventonentry == 91)
+					game.switchstage.eventonentry == 91)
 			{
 				freshstart = true;
 			}
-			
+
 			// switch maps
 			if (load_stage(game.switchstage.mapno)) goto ingame_error;
-			
+
 			player->x = (game.switchstage.playerx * TILE_W) << CSF;
 			player->y = (game.switchstage.playery * TILE_H) << CSF;
 		}
-		
+
 		// start the level
 		if (game.initlevel()) return 1;
-		
+
 		if (freshstart)
 			weapon_introslide();
-		
+
 		gameloop();
 		game.stageboss.OnMapExit();
 		freshstart = false;
-	}
-	
+	}while(game.running);
+
 shutdown: ;
-	Replay::close();
-	game.close();
-	Carets::close();
-	
-	Graphics::close();
-	input_close();
-	font_close();
-	sound_close();
-	tsc_close();
-	textbox.Deinit();
-	return error;
-	
+	  Replay::close();
+	  game.close();
+	  Carets::close();
+
+	  Graphics::close();
+	  input_close();
+	  font_close();
+	  sound_close();
+	  tsc_close();
+	  textbox.Deinit();
+	  return error;
+
 ingame_error: ;
-	stat("");
-	stat(" ************************************************");
-	stat(" * An in-game error occurred. Game shutting down.");
-	stat(" ************************************************");
-	error = true;
-	goto shutdown;
+	      stat("");
+	      stat(" ************************************************");
+	      stat(" * An in-game error occurred. Game shutting down.");
+	      stat(" ************************************************");
+	      error = true;
+	      goto shutdown;
 }
 
 
 void gameloop(void)
 {
-uint32_t gametimer;
+	uint32_t gametimer;
 
 	gametimer = -GAME_WAIT*10;
 	game.switchstage.mapno = -1;
-	
+
 	while(game.running && game.switchstage.mapno < 0)
 	{
 		uint32_t curtime = SDL_GetTicks();
-		
+
+		#ifdef DEBUG
 		if ((curtime - gametimer >= GAME_WAIT) || game.ffwdtime)
+		#else
+		if ((curtime - gametimer >= GAME_WAIT))
+		#endif
 		{
 			run_tick();
-			
+
 			// try to "catch up" if something else on the system bogs us down for a moment.
 			// but if we get really far behind, it's ok to start dropping frames
+			#ifdef DEBUG
 			if (game.ffwdtime)
 			{
 				gametimer = curtime;
 				game.ffwdtime--;
 			}
-			else if ((curtime - gametimer > (GAME_WAIT * 3)))
+			else
+			#endif
+			if ((curtime - gametimer > (GAME_WAIT * 3)))
 			{
 				gametimer = curtime;
 			}
@@ -232,13 +244,15 @@ uint32_t gametimer;
 
 static inline void run_tick()
 {
-static bool can_tick = true;
-static bool last_freezekey = false;
-static bool last_framekey = false;
-static int frameskip = 0;
+#ifdef DEBUG
+	static bool can_tick = true;
+#endif
+	static bool last_freezekey = false;
+	static bool last_framekey = false;
+	static int frameskip = 0;
 
 	input_poll();
-	
+
 	// input handling for a few global things
 	if (justpushed(ESCKEY))
 	{
@@ -255,9 +269,9 @@ static int frameskip = 0;
 	{
 		game.pause(GP_OPTIONS);
 	}
-	
+
 	// freeze frame
-	#ifdef DEBUG
+#ifdef DEBUG
 	if (settings->enable_debug_keys)
 	{
 		if (inputs[FREEZE_FRAME_KEY] && !last_freezekey)
@@ -266,7 +280,7 @@ static int frameskip = 0;
 			freezeframe ^= 1;
 			framecount = 0;
 		}
-		
+
 		if (inputs[FRAME_ADVANCE_KEY] && !last_framekey)
 		{
 			can_tick = true;
@@ -276,24 +290,27 @@ static int frameskip = 0;
 				framecount = 0;
 			}
 		}
-		
+
 		last_freezekey = inputs[FREEZE_FRAME_KEY];
 		last_framekey = inputs[FRAME_ADVANCE_KEY];
 	}
-	#endif
-	
-	#ifdef DEBUG
+#endif
+
+#ifdef DEBUG
 	// fast-forward key (F5)
 	if (inputs[FFWDKEY] && (settings->enable_debug_keys || Replay::IsPlaying()))
 	{
 		game.ffwdtime = 2;
 	}
-	#endif
-	
+#endif
+
+#ifdef DEBUG
 	if (can_tick)
 	{
+#endif
 		game.tick();
-		
+
+#ifdef DEBUG
 		if (freezeframe)
 		{
 			char buf[1024];
@@ -303,16 +320,17 @@ static int frameskip = 0;
 		}
 		else
 		{
+#endif
 			Replay::DrawStatus();
+#ifdef DEBUG
 		}
-		
-		#ifdef DEBUG
+
 		if (settings->show_fps)
 		{
 			update_fps();
 		}
-		#endif
-		
+#endif
+
 		if (!flipacceltime)
 		{
 			screen->Flip();
@@ -326,14 +344,16 @@ static int frameskip = 0;
 				frameskip = 256;
 			}
 		}
-		
+
 		memcpy(lastinputs, inputs, sizeof(lastinputs));
-	}
+#ifdef DEBUG
+	} 
 	else
 	{	// frame is frozen; don't hog CPU
 		SDL_Delay(10);
 	}
-	
+#endif
+
 	// immediately after a game tick is when we have the most amount of time before
 	// the game needs to run again. so now's as good a time as any for some
 	// BGM audio processing, wouldn't you say?
