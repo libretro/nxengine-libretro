c792f5674e0bec61cedf0878b883690827f2e5e5
Twinaphex <autechre1024@hotmail.com>
2012-04-12 20:30:47 +0200

    Revert "Revert "add SSNES_SetClipRect - replace calls to SDL_SetClipRect""
    This reverts commit 2f57d978e177720883b883c0f75b95e8692339f4.

--- graphics/sdl_wrapper.h
+++ graphics/sdl_wrapper.h
@@ -7,6 +7,7 @@ extern "C" {
 
 int SSNES_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, Uint32 color);
 int SSNES_SetColorKey (SDL_Surface *surface, Uint32 flag, Uint32 key);
+SDL_bool SSNES_SetClipRect(SDL_Surface *surface, const SDL_Rect *rect);
 
 #ifdef __cplusplus
 }
