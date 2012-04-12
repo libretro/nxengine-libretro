dc3b4874caa664ab7324cb592eddeed81121780e
Twinaphex <autechre1024@hotmail.com>
2012-04-12 18:12:47 +0200

    Add libretro mention to title screen

--- intro/title.cpp
+++ intro/title.cpp
@@ -245,7 +245,7 @@ static void draw_title()
 	draw_sprite(cx, acc_y, SPR_PIXEL_FOREVER);
 	
 	// version
-	static const char *VERSION = "NXEngine v. 1.0.0";
+	static const char *VERSION = "NXEngine v. 10.0.3 (libretro)";
 	static const int SPACING = 5;
 	int wd = GetFontWidth(VERSION, SPACING);
 	cx = (SCREEN_WIDTH / 2) - (wd / 2);
