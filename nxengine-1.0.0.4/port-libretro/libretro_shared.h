97494e53180fcc53c7bdb7fbd810d71b3eeb0f37
twinaphex <autechre1024@hotmail.com>
2013-01-14 07:43:59 +0100

    (MSVC 2003 Xbox 1) Some fixes

--- nxengine-1.0.0.4/port-libretro/libretro_shared.h
+++ nxengine-1.0.0.4/port-libretro/libretro_shared.h
@@ -1,6 +1,10 @@
 #ifndef _LIBRETRO_SHARED_H
 #define _LIBRETRO_SHARED_H
 
+#ifdef _WIN32
+#define snprintf _snprintf
+#endif
+
 void retro_create_subpath_string(char *fname, size_t fname_size, const char * dir, const char * subdir, const char * filename);
 void retro_create_path_string(char *fname, size_t fname_size, const char * dir, const char * filename);
 
