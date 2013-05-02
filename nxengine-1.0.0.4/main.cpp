7d23ce484c85e18facd837e1ed059b0861f7c7ed
twinaphex <autechre1024@hotmail.com>
2013-05-02 02:24:38 +0200

    Revert "Put in functions to read entire file into memory"
    This reverts commit 387a3f0e5e5ae56a3c4658af401ff185a2aea6c1.

--- nxengine-1.0.0.4/main.cpp
+++ nxengine-1.0.0.4/main.cpp
@@ -30,7 +30,6 @@ static bool error = false;
 static bool freshstart;
 
 extern bool extract_files(FILE *exefp);
-extern bool extract_data_files(void);
 extern bool extract_stages(FILE *exefp);
 
 void pre_main(void)
@@ -65,8 +64,6 @@ SetLogFilename(debug_fname);
 	
    settings->files_extracted = true;
    settings_save();
-
-   extract_data_files();
 	
 	if (Graphics::init(settings->resolution)) { NX_ERR("Failed to initialize graphics.\n"); error = 1; return; }
 	if (font_init()) { NX_ERR("Failed to load font.\n"); error = 1; return; }
