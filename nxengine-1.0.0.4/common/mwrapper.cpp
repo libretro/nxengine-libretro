8b65ece843775884d662b7faf99d33a946736496
twinaphex <autechre1024@hotmail.com>
2013-05-02 02:24:38 +0200

    Revert "Put in functions to read entire file into memory"
    This reverts commit 411e3f3edc32a84dfe978ca9a8cf0d0078e81419.

--- nxengine-1.0.0.4/common/mwrapper.cpp
+++ nxengine-1.0.0.4/common/mwrapper.cpp
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
 #include <stdint.h>
-#include <sys/stat.h>
 #include "mwrapper.h"
 
 int mgetc(char **fp)
@@ -32,54 +28,3 @@ uint32_t mgetl(char **fp)
 	return (d<<24)|(c<<16)|(b<<8)|(a);
 }
 
-/* This routine returns the size of the file it is called with. */
-
-unsigned get_file_size (const char * file_name)
-{
-   struct stat sb;
-   if (stat (file_name, & sb) != 0) {
-      fprintf (stderr, "'stat' failed for '%s': %s.\n",
-            file_name, strerror (errno));
-      exit (EXIT_FAILURE);
-   }
-   return sb.st_size;
-}
-
-/* This routine reads the entire file into memory. */
-
-unsigned char *read_whole_file (const char * file_name)
-{
-   unsigned s;
-   unsigned char * contents;
-   FILE * f;
-   size_t bytes_read;
-   int status;
-
-   s = get_file_size (file_name);
-   contents = (unsigned char*)malloc (s + 1);
-   if (! contents) {
-      fprintf (stderr, "Not enough memory.\n");
-      return NULL;
-   }
-
-   f = fopen (file_name, "r");
-   if (! f) {
-      fprintf (stderr, "Could not open '%s': %s.\n", file_name,
-            strerror (errno));
-      return NULL;
-   }
-   bytes_read = fread (contents, sizeof (unsigned char), s, f);
-   if (bytes_read != s) {
-      fprintf (stderr, "Short read of '%s': expected %d bytes "
-            "but got %d: %s.\n", file_name, s, bytes_read,
-            strerror (errno));
-      return NULL;
-   }
-   status = fclose (f);
-   if (status != 0) {
-      fprintf (stderr, "Error closing '%s': %s.\n", file_name,
-            strerror (errno));
-      return NULL;
-   }
-   return contents;
-}
--- nxengine-1.0.0.4/common/mwrapper.h
+++ nxengine-1.0.0.4/common/mwrapper.h
@@ -5,7 +5,4 @@ int mgetc(char **fp);
 uint16_t mgeti(char **fp);
 uint32_t mgetl(char **fp);
 
-unsigned get_file_size (const char * file_name);
-unsigned char *read_whole_file (const char * file_name);
-
 #endif
