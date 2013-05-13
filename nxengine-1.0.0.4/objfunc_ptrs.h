2f07157d5a6f580b5372fe8a76cb6672b2597304
twinaphex <autechre1024@hotmail.com>
2013-05-13 02:38:23 +0200

    (XDK) Implement more AI routines

--- nxengine-1.0.0.4/objfunc_ptrs.h
+++ nxengine-1.0.0.4/objfunc_ptrs.h
@@ -31,6 +31,55 @@ extern void ai_snake_trail(Object *o);
 /* sym */
 extern void ai_smokecloud(Object *o);
 
+extern void ai_null(Object *o);
+extern void ai_hvtrigger(Object *o);
+extern void ai_xp(Object *o);
+extern void ai_powerup(Object *o);
+extern void ai_hidden_powerup(Object *o);
+extern void ai_xp_capsule(Object *o);
+extern void ai_save_point(Object *o);
+extern void ai_recharge(Object *o);
+extern void ai_chest_closed(Object *o);
+extern void ai_chest_open(Object *o);
+extern void ai_lightning(Object *o);
+extern void ai_teleporter(Object *o);
+extern void ai_door(Object *o);
+extern void ai_largedoor(Object *o);
+extern void ai_press(Object *o);
+extern void ai_terminal(Object *o);
+extern void ai_fan_vert(Object *o);
+extern void ai_fan_hoz(Object *o);
+extern void ai_fan_droplet(Object *o);
+extern void ai_sprinkler(Object *o);
+extern void ai_droplet_spawner(Object *o);
+extern void ai_water_droplet(Object *o);
+extern void ai_bubble_spawner(Object *o);
+extern void ai_chinfish(Object *o);
+extern void ai_fireplace(Object *o);
+extern void ai_straining(Object *o);
+extern void ai_smoke_dropper(Object *o);
+extern void ai_scroll_controller(Object *o);
+extern void ai_quake(Object *o);
+extern void ai_generic_angled_shot(Object *o);
+extern void onspawn_spike_small(Object *o);
+extern void ai_animate2(Object *o);
+extern void ai_animate4(Object *o);
+
+
+/* oside */
+extern void ai_sky_dragon(Object *o);
+extern void ai_sandcroc(Object *o);
+extern void ai_night_spirit(Object *o);
+extern void ai_night_spirit_shot(Object *o);
+extern void ai_hoppy(Object *o);
+extern void ai_pixel_cat(Object *o);
+extern void ai_little_family(Object *o);
+
+/* ai/npc/npcplayer */
+extern void ai_npc_player(Object *o);
+extern void ai_ptelin(Object *o);
+extern void ai_ptelout(Object *o);
+
 /* AFTERMOVE */
 
 extern void aftermove_blade_l12_shot(Object *o);
