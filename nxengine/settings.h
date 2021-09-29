505c74421898b93640b313cde48e47ffdd439fe3
twinaphex <libretro@gmail.com>
2021-09-29 14:15:49 +0200

    Revert "Cleanups"
    This reverts commit c32c9b214ea46649ce8947dfd4a431a5023adb81.

--- nxengine/settings.h
+++ nxengine/settings.h
@@ -11,14 +11,17 @@ struct Settings
 	int last_save_slot;
 	bool multisave;
 	bool files_extracted;
+	bool show_fps;
 	bool displayformat;
 	
+	bool enable_debug_keys;
 	bool sound_enabled;
 	int music_enabled;
 	
 	bool instant_quit;
 	bool emulate_bugs;
 	bool no_quake_in_hell;
+	bool inhibit_fullscreen;
 	
 	bool skip_intro;
 	int reserved[8];
